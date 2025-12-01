361da1e7e6f3b518245ec633570df61d36850647
Manuel Calixto <calixtopinar@gmail.com>
2025-12-01 12:40:02 +0100

    Lista de la compra React con JSON

--- REACT/ListaCompra/src/ListaCompra.jsx
+++ REACT/ListaCompra/src/ListaCompra.jsx
@@ -3,22 +3,32 @@ import Item from './Item.jsx';
 import Pie from './Pie.jsx';
 
 export default function ListaCompra(){
-    const [listaCompra,setListaCompra] = useState(["pan","azucar"]);
+    let datosIniciales = {"items":[
+        {
+            "nombre":"pan",
+            "cantidad":"2"
+        },
+        {
+            "nombre":"azucar",
+            "cantidad":"1"
+        }
+    ]}
+    const [listaCompra,setListaCompra] = useState(datosIniciales.items);
     const [contenido,setContenido] = useState("");
 
     function añadirElemento(){
-        setListaCompra([...listaCompra,contenido]);
+        setListaCompra([...listaCompra,{"nombre":contenido}]);
         setContenido("");
     }
 
     function actualizarElemento(nuevoContenido,id){
         const nuevaLista = [...listaCompra];
-        nuevaLista[id] = nuevoContenido;
+        nuevaLista[id] ={"nombre":nuevoContenido};
         setListaCompra(nuevaLista);
     }
 
     function eliminarElemento(indice){
-        const nuevaLista = [...listaCompra] 
+        const nuevaLista = [...listaCompra]; 
         nuevaLista.splice(indice,1);
         setListaCompra(nuevaLista);
     }
@@ -28,7 +38,8 @@ export default function ListaCompra(){
     }
 
     let maqueta = listaCompra.map((item,indice)=> <Item key={indice}
-            nombre={item}
+            nombre={item.nombre}
+            cantidad={item.cantidad}
             id={indice}
             onNuevoTexto={actualizarElemento}
             onDelete={eliminarElemento}></Item>)
--- TEMA 5/Relacion3/Grocery_list/dom.js
+++ TEMA 5/Relacion3/Grocery_list/dom.js
@@ -1,25 +1,42 @@
+function creaVistaNota(elemento){
+    cont = document.getElementById("lista");
+    li = document.createElement("li");
+    li.innerHTML = elemento;
+    cont.appendChild(li);
+}
+
 window.onload = () => {
     var lista = document.getElementById("lista");
+    lista = JSON.parse(localStorage.getItem("lista"));
+    if(lista){
+        for(elemento of lista){
+            creaVistaNota(elemento);
+        }
+    }else
+        lista = [];
     var mibtn = document.getElementById("miBtn");
 
     mibtn.addEventListener("click",()=>{
         let contenidoInput = document.getElementById("input").value;
+        lista.push(contenidoInput);
         let nuevoLi = document.createElement("li");
         let nuevoBtnBorrar = document.createElement("button");
         let nuevoBtnEditar = document.createElement("button");
-        nuevoLi.innerHTML = contenidoInput;
+        nuevoLi.innerHTML = lista[lista.length-1];
         lista.appendChild(nuevoLi);
         nuevoLi.appendChild(nuevoBtnBorrar);
         nuevoLi.appendChild(nuevoBtnEditar);
         nuevoBtnEditar.innerHTML = "Editar";
         nuevoBtnBorrar.innerHTML = "Borrar";
+        localStorage.setItem("lista",JSON.stringify(lista));
         document.getElementById("input").value = "";
-        nuevoBtnEditar.addEventListener("click",(e) => {
+
+        nuevoBtnEditar.addEventListener("click",() => {
         texto = prompt("Introduce el nuevo texto:");
         nuevoLi.innerText = texto;
         })
 
-        nuevoBtnBorrar.addEventListener("click",(e) => {
+        nuevoBtnBorrar.addEventListener("click",() => {
             if(confirm("¿Seguro que deseas borrar esta entrada?")){
                 lista.removeChild(nuevoLi);
             }
@@ -27,7 +44,7 @@ window.onload = () => {
     })
 
     var mibtnBorrar = document.getElementById("clear");
-    mibtnBorrar.addEventListener("click", (e)=>{
+    mibtnBorrar.addEventListener("click", ()=>{
 
         if(confirm("¿Seguro que deseas borrar la lista entera? ")){
             lista.innerHTML = "";
